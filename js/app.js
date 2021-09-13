eb531cb61d6369227eadc909ca6187488485f40d
ohidulalam <mdnoyonctg15@gmail.com>
2021-09-13 21:19:54 +0600

    final added

--- js/app.js
+++ js/app.js
@@ -16,15 +16,15 @@ const showProducts = (products) => {
     const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card shadow mb-3 bg-body p-3">
+        <div class="card shadow mb-3 bg-card p-3">
           <img src="${product.image}"class="card-img-top img-fluid w-50 mx-auto" alt="...">
           <div class="card-body">
             <h4 class= "fw-bold">${product.title}</h4>
             <p><b>Category:</b> ${product.category}</p>
             <p><b> <i class="fas fa-star"></i> Rating Rate:</b> ${product.rating.rate}, <b> <i class="fas fa-american-sign-language-interpreting"></i> Rating count:</b> ${product.rating.count}</p>
             <h2><b>Price:</b> $${product.price}</h2>
-            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">Add to cart</button>
-            <button id="details-btn" class="btn btn-dark">Details</button>
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-danger">Add to cart</button>
+            <button id="details-btn" class="btn btn-success">Details</button>
           </div>
         </div>
         `;
